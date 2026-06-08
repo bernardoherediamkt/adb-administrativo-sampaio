406be73ed55b3e3d7790353ca9a892f6a0f03146
bernardoherediamkt <bernardoheredia.mkt@gmail.com>
2026-06-08 17:48:04 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'adb-administrativo-midia-designer-upload-v2';
+const CACHE_NAME = 'adb-administrativo-midia-designer-botoes-corrigidos-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
